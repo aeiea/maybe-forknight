071c796284f5ddbbb6fbeeff952df2db128ce764
aeiea <118844761+aeiea@users.noreply.github.com>
2023-04-25 16:49:58 -0700

    dsaasdd

--- script.js
+++ script.js
@@ -1,21 +1,23 @@
-function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
-var proxy_messages = [
-    "loading proxy.",
-    "loading proxy..",
-    "loading proxy...",
-    "proxy loaded.",
-    "proxy loaded..",
-    "proxy loaded...",
-    "requesting connection.",
-    "requesting connection..",
-    "requesting connection...",
-    "connection requested.",
-    "done."
-];
-var proxy_loading = document.getElementById("proxy-loading");
-for (let i = 0; i < proxy_messages.length; i++) {
-    proxy_loading.innerHTML = proxy_messages[i];
-    await sleep(1000);
+async function proxymsg() {
+    async function sleep(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
+    var proxy_messages = [
+        "loading proxy.",
+        "loading proxy..",
+        "loading proxy...",
+        "proxy loaded.",
+        "proxy loaded..",
+        "proxy loaded...",
+        "requesting connection.",
+        "requesting connection..",
+        "requesting connection...",
+        "connection requested.",
+        "done."
+    ];
+    var proxy_loading = document.getElementById("proxy-loading");
+    for (let i = 0; i < proxy_messages.length; i++) {
+        proxy_loading.innerHTML = proxy_messages[i];
+        await sleep(1000);
+    }
 }
