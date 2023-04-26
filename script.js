2755be845a3bf631704b9964cbee042de37f0042
Kate Mazdyk <dpthn@proton.me>
2023-04-26 16:05:41 +0000

    wdqfwesdvsdv

--- script.js
+++ script.js
@@ -28,6 +28,20 @@ async function fake_forknight(){
     }
     document.getElementById("fortnite-loading").classList.remove("hidden");
     document.getElementById("fake-proxy").classList.add("hidden");
-    var fortnite_loading_image = '/fork.png';
-
+    var i = 0;
+    if (i == 0) {
+        i = 1;
+        var elem = document.getElementById("fortnite-loading-bar-fill");
+        var width = 1;
+        var id = setInterval(frame, Math.floor(Math.random() * 50) + 1);
+        function frame() {
+            if (width >= 100) {
+                clearInterval(id);
+                i = 0;
+            } else {
+                width++;
+                elem.style.width = width + "%";
+            }
+        }
+    }
 }
