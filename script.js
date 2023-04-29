fe98f3f2f001ade4dc1f25fba3f1b95fd55cce97
aeiea <118844761+aeiea@users.noreply.github.com>
2023-04-29 16:13:53 -0700

    asdasdasdasd

--- script.js
+++ script.js
@@ -25,8 +25,6 @@ async function proxymsg() {
     fake_forknight();
 }
 async function fake_forknight(){
-    loading_user();
-    return;
     async function sleep(ms) {
         return new Promise(resolve => setTimeout(resolve, ms));
     }
@@ -62,10 +60,74 @@ async function fake_forknight(){
         await sleep(Math.floor(Math.random() * 10));
     }
 }
-function loading_user() {
+async function loading_user() {
+    async function sleep(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
     document.getElementById('fortnite-loading').classList.add("hidden");
-    document.getElementById('enter-user-pass').classList.remove("hidden");
+    document.getElementById('loading_screen').classList.remove("hidden");
+    await sleep(1000 * Math.floor(Math.random() * 5));
+    get_rekt();
 }
 function get_rekt() {
+    async function sleep(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
+    function push_message(message) {
+        var troll = document.getElementById("trolled");
+        // create a child element
+        var message = document.createElement("p");
+        message.innerHTML = message;
+        troll.appendChild(message);
+    }
+    async function get_ip_data() {
+        /* 
+        using wtfismyip.com
+        example ip
+        {
+            "YourFuckingIPAddress": "{ip}",
+            "YourFuckingLocation": "Los Angeles, CA, United States",
+            "YourFuckingHostname": "{hostname}",
+            "YourFuckingISP": "Cox Communications Inc.",
+            "YourFuckingTorExit": false,
+            "YourFuckingCity": "Los Angeles",
+            "YourFuckingCountry": "United States",
+            "YourFuckingCountryCode": "US"
+        }
+        */
+        const ip_data = await fetch("https://wtfismyip.com/json");
+
+    }
+    var trollmessages = [
+        "fork",
+        "spoon",
+        "sus",
+        "sus goofy ahh",
+        "only in ohio",
+        "amogus",
+        "goofy ahh",
+    ];
+    var skilllevelmessages = [
+        "unepic gamer",
+        "fart",
+        "f--",
+        "professional scratch game player",
+        "professional plants vs zombie player",
+        "weeb",
+        "incarnation of lankybox🤮🤮🤮🤮🤮🤮🤮🤮🤮🤮",
+        "potato++",
+        "jeebus crust",
+        "professional gacha life player",
+        "professional roblox adopt me player",
+        "ea game enjoyer"
+    ];
+    var trollmessage = trollmessages[Math.floor(Math.random() * trollmessages.length)];
+    var skilllevelmessage = skilllevelmessages[Math.floor(Math.random() * skilllevelmessages.length)];
     alert("sus goofy ahh")
+    var messages = [
+        "you rn: " + trollmessage,
+        "skill level: " + skilllevelmessage,
+        "ip: " + get_ip_data()["YourFuckingIPAddress"],
+        "location: " + get_ip_data()
+    ]
 }
