e692db94e2a72c66dde39a68eea7c35d9cd3dae8
aeiea <118844761+aeiea@users.noreply.github.com>
2023-05-08 11:20:21 -0700

    sus

--- .github/workflows/autoupdater.py
+++ .github/workflows/autoupdater.py
@@ -2,22 +2,18 @@
 import os, subprocess, shutil
 
 # ⚙️ CONFIG
-repo_to_update = "https://github.com/aeiea/autoupdateonfolder-workflow.git" # put your repo's github thingy
-folder_to_check_for_updates = "test" # checks the folder test for updates
-file_to_update = "listoffiles.txt" # list of files in the directory test
+repo_to_update = "https://github.com/aeiea/maybe-forknight.git" # put your repo's github thingy
+folder_to_check_for_updates = "videos" # checks the folder test for updates
+file_to_update = "videos.txt" # list of files in the directory test
 
 # 🐍 CODE
-try:
-      gh_key = os.environ["API_KEY"]
-except:
-      print("Hey! you might of forgotten your github API key!")
 print('''
        ,            .    ,       
  _.. .-+- _ . .._  _| _.-+- _ ._.
 (_](_| | (_)(_|[_)(_](_] | (/,[  
                |                 
           by @aeiea
- used on {your username}/{repo}
+ used on aeiea/maybe-forknight
 ''')
 subprocess.run("git clone --depth=1 " + repo_to_update + " thisfolderisgoingtobedeleted && rm -rf thisfolderisgoingtobedeleted/.git", shell=1)
 files = [f for f in os.listdir("thisfolderisgoingtobedeleted/" + folder_to_check_for_updates + "/") if os.path.isfile(os.path.join("thisfolderisgoingtobedeleted/" + folder_to_check_for_updates + "/", f))]
